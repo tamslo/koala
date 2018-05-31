3c51a8a03dd71d6414556d69623b465076f3eec7
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-05-31 10:05:22 +0200

    Build services in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,15 @@
+# Install Python requirements
 pip install -r requirements.txt
+
+# Install Node requirements
 cd client
 npm install
+cd ..
+
+# Build services as Docker images
+cd services
+for dockerfile in ./Dockerfile.*; do
+  image_name=${dockerfile#"./Dockerfile."}
+  docker build -t $image_name -f $dockerfile .
+done
 cd ..
