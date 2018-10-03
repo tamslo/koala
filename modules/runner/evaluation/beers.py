3e4c6254ec951ce317a4703faca0591698be8d8d
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-10-03 22:40:50 +0200

    [WIP] Add BEERS evaluation to runner

---
+++ modules/runner/evaluation/beers.py
@@ -0,0 +1,14 @@
+# Convert BAM to sorted SAM if needed
+# samtools view merged.bam | sort -t'.' -k 2n > output.sam
+
+# Fix SAM
+# ruby aligner_benchmark/fix_sam.rb output.sam > fixed.sam
+
+# Compare to truth
+# perl aligner_benchmark/perl_scripts/compare2truth.pl CIGAR_PATH fixed.sam -noHtag > comp_res.txt
+
+# Infer junctions
+# perl aligner_benchmark/perl_scripts/sam2junctions.pl fixed.sam > inferred_junctions.txt
+
+# Junction stats
+# perl /Users/hayer/github/aligner_benchmark/perl_scripts/compare_junctions_in_simulated_to_INFERRED_junctions.pl PATH_TO_TRANSCRIPTS JUNCTIONS_CROSSED_PATH inferred_junctions.txt junctions > junctions_stats.txt
