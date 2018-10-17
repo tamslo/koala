87f3e115742a96fd7af3aa9fe7c6185ea42e31d7
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-10-17 12:28:16 +0200

    Fix base aligner and remove debugging

--- services/base_aligner.py
+++ services/base_aligner.py
@@ -29,6 +29,7 @@ class BaseAligner(BaseService):
     def __run_with_file_creation(self, parameters, command):
         docker_client = parameters["docker_client"]
         destination = parameters["destination"]
+
         container = docker_client.run(
             self.id,
             command,
@@ -37,51 +38,19 @@ class BaseAligner(BaseService):
             auto_remove=False
         )
 
-        # 8<-- DEBUGGING
-        lines = 0
-        if self.id == "novoalign":
-            print("Started alignment, conainer id is {}".format(container.id))
-        # DEBUGGING -->8
-
         out_file_path = os.path.join(destination, "Out.sam")
         out_file = open(out_file_path, "wb")
         for line in container.logs(stdout=True, stderr=False, stream=True):
-            # 8<-- DEBUGGING
-            lines += 1
-            # DEBUGGING -->8
             out_file.write(line)
         out_file.close()
-
-        # 8<-- DEBUGGING
-        if self.id == "novoalign":
-            print("Done writing SAM file, wrote {} lines".format(lines))
-        lines = 0
-        # DEBUGGING -->8
 
         log_file_path = os.path.join(destination, "Out.log")
         log_file = open(log_file_path, "wb")
         for line in container.logs(stdout=False, stderr=True, stream=True):
-            # 8<-- DEBUGGING
-            lines += 1
-            # DEBUGGING -->8
             log_file.write(line)
         log_file.close()
 
-        # 8<-- DEBUGGING
-        if self.id == "novoalign":
-            print("Done writing Log file, wrote {} lines".format(lines))
-        # DEBUGGING -->8
-
-        # 8<-- DEBUGGING
-        if self.id == "novoalign":
-            print("Container status before refreshing: {}".format(container.status))
-        # DEBUGGING -->8
-        container.refresh()
-        # 8<-- DEBUGGING
-        if self.id == "novoalign":
-            print("Container status after refreshing: {}".format(container.status))
-        # DEBUGGING -->8
-
+        container.reload()
         if container.status != "exited":
             container.stop()
         container.remove()
