a4124d80cece4d16747abcdfb8916c5b2e3fc201
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-10-22 20:01:41 +0200

    Improve remove data script

--- scripts/remove-data.sh
+++ scripts/remove-data.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-rm -r data/experiments
+rm -rf data/experiments
 
 cd data/datasets
 for dataset_json in *.json; do
   dataset_directory=${dataset_json%".json"}
-  if [[ $dataset_directory =~ ^prepared.* ]]; then
-    for cache_directory in $dataset_directory/*; do
-      directory_name=${cache_directory#"$dataset_directory/"}
-      [ "$directory_name" == "pfal" ] && is_pfal=true || is_pfal=false
-      [ "$directory_name" == "hg19" ] && is_hg19=true || is_hg19=false
-      [ "$directory_name" == "hg38" ] && is_hg38=true || is_hg38=false
-      if $is_pfal || $is_hg19 || $is_hg38; then
-        rm -rf $cache_directory
-      fi
-    done
-  else
-    rm $dataset_json
-    rm -r $dataset_directory
+  for cache_directory in $dataset_directory/*; do
+    directory_name=${cache_directory#"$dataset_directory/"}
+    [ "$directory_name" == "pfal" ] && is_pfal=true || is_pfal=false
+    [ "$directory_name" == "hg19" ] && is_hg19=true || is_hg19=false
+    [ "$directory_name" == "hg38" ] && is_hg38=true || is_hg38=false
+    if $is_pfal || $is_hg19 || $is_hg38; then
+      rm -rf $cache_directory
+    fi
+  done
+done
+
+cd ../references
+for file in ./*; do
+  if [[ $file == *_* ]]
+  then
+    rm -r $file
   fi
 done
