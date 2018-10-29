ee7ce281ccdf188cdeedbb5f95a8883c6054b164
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-10-29 10:06:33 +0100

    Uncomment install steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,12 @@
-# # Install Python requirements
-# pip install -r requirements.txt --no-cache-dir
-#
-# # Install Node requirements
-# cd client
-# npm install
-# cd ..
+# Install Python requirements
+pip install -r requirements.txt --no-cache-dir
+
+# Install Node requirements
+cd client
+npm install
+cd ..
 
 bash scripts/build-docker.sh
-# 
-# # Download data
-# python scripts/download-data.py
+
+# Download data
+python scripts/download-data.py
