192b640c548e2856e03832eda40c3861e80b94ce
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-11-16 18:48:41 +0100

    Revert "Remove softClipping option from Opossum"
    This reverts commit 7634423c3daaa056c5f09251f7fbee080530d408.

--- services/opossum/__init__.py
+++ services/opossum/__init__.py
@@ -4,11 +4,14 @@ from ..base_service import BaseService
 class Opossum(BaseService):
     def run(self, parameters):
         experiment = parameters["experiment"]
+        preceding_service = experiment.get_preceding_service(self.id)
         in_file_path = experiment.get_input_directory(self.id) + "Out.bam"
         out_file_path = parameters["destination"] + "Out.bam"
-        command = "python Opossum.py --BamFile=/{} --OutFile=/{}".format(
+        soft_clips_exist = preceding_service.soft_clips_exist
+        command = "python Opossum.py --BamFile=/{} --OutFile=/{} --SoftClipsExist={}".format(
             in_file_path,
-            out_file_path
+            out_file_path,
+            soft_clips_exist
         )
         self.run_docker(command, parameters)
         file_utils.validate_file_content(out_file_path)
