3f42ea79fe18f47119f47dfbee8dbf02428317a5
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-11-28 07:12:27 +0100

    Move NovoAlign for FASTA input to production services

--- services/__init__.py
+++ services/__init__.py
@@ -33,11 +33,11 @@ ServiceClasses = {
 if environment == "test":
     ServiceClasses["test_aligner_writes_file"] = TestAlignerWritesFile
     ServiceClasses["test_aligner_writes_log"] = TestAlignerWritesLog
-    ServiceClasses["novoalign_fa"] = NovoAlignFasta
 
 if environment == "production":
     ServiceClasses["star"] = Star
     ServiceClasses["novoalign"] = NovoAlign
+    ServiceClasses["novoalign_fa"] = NovoAlignFasta
     ServiceClasses["novoalign_tweaked"] = NovoAlignIndelSensitive
 
 def get_services():
