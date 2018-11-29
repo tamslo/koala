9bb5294595dc9b8f43c2c37f8f4883a83409b29e
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-11-29 22:24:22 +0100

    Attempt to fix GIAB evaluation again

--- services/giab/assets/evaluate_variants.sh
+++ services/giab/assets/evaluate_variants.sh
@@ -9,7 +9,8 @@ additional_commands="$4"
 cd $happy_directory
 
 HGREF=/data/references/$reference_id.fa \
-  ./hap.py $additional_commands /giab/$reference_id/confidence_calls.vcf \
+  ./hap.py /giab/$reference_id/confidence_calls.vcf \
   $path_prefix/$vcf_file_name \
   -f /giab/$reference_id/confidence_calls.bed \
-  -o $path_prefix/Evaluation
+  -o $path_prefix/Evaluation \
+  $additional_commands
