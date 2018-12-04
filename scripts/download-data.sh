b2f1771bb95731824e50153bf4be8ee09a974cad
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-12-05 00:16:27 +0100

    Restrict annotation data to standard chromosomes

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -1,11 +1,19 @@
-# Extract annotations
+# Extract annotations (only for standard chromosomes)
 if [ ! -f data/annotations/hg19.txt ]; then
-  gunzip -c data/annotations/refFlat.hg19.txt.gz > data/annotations/hg19.txt
-  gunzip -c data/annotations/refFlat.hg38.txt.gz > data/annotations/hg38.txt
+  gunzip -c data/annotations/refFlat.hg19.txt.gz | grep -v 'chr.*_' > data/annotations/hg19.txt
+  gunzip -c data/annotations/refFlat.hg38.txt.gz | grep -v 'chr.*_' > data/annotations/hg38.txt
 fi
 
-# Download references for single chromosomes
+# Download references for single chromosomes (and delete non-standard)
 if [ ! -d data/references/hg19 ]; then
   wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/* -P data/references/hg19
   wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/* -P data/references/hg38
+
+  cd data/references/hg19
+  find . -type f ! -name '*.fa.gz' -delete
+  rm -f *_*.fa.gz
+
+  cd ../hg38
+  find . -type f ! -name '*.fa.gz' -delete
+  rm -f *_*.fa.gz
 fi
