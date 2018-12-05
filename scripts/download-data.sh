6043ffbcb1fd0bdd53bf3ee266a1c067b94360ab
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-12-05 16:59:17 +0100

    Another fix

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -7,15 +7,13 @@ fi
 # Download references for single chromosomes (and delete non-standard)
 if [ ! -d data/references/hg19 ]; then
   current_dir=$pwd
-  wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/* -P data/references/hg19
-  wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/* -P data/references/hg38
+  wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/*.fa.gz -P data/references/hg19
+  wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/*.fa.gz -P data/references/hg38
 
   cd ~/code/data/references/hg19
-  find . -type f ! -name '*.fa.gz' -delete
   rm -f *_*.fa.gz
 
   cd ~/code/data/references/hg38
-  find . -type f ! -name '*.fa.gz' -delete
   rm -f *_*.fa.gz
   cd $current_dir
 fi
