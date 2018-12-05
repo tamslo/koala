5027781528cd23c3f72093ebd1c0bff99c981b86
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-12-05 18:02:57 +0100

    Adapt directories in download data for general use cases

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -1,3 +1,4 @@
+
 # Extract annotations (only for standard chromosomes)
 if [ ! -f data/annotations/hg19.txt ]; then
   echo "Parsing annotations"
@@ -11,16 +12,10 @@ fi
 # Download references for single chromosomes (and delete non-standard)
 if [ ! -d data/references/hg19 ]; then
   echo "Downloading reference FASTAs per chromosome"
-  current_dir=$pwd
   wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/*.fa.gz -P data/references/hg19
   wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/*.fa.gz -P data/references/hg38
-
-  cd ~/code/data/references/hg19
-  rm -f *_*.fa.gz
-
-  cd ~/code/data/references/hg38
-  rm -f *_*.fa.gz
-  cd $current_dir
+  rm -f data/references/hg19/*_*.fa.gz
+  rm -f data/references/hg38/*_*.fa.gz
   echo "Done"
 else
   echo "FASTAs per chromosome already present"
