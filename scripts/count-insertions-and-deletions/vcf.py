7f15990450ade4bca36455b711803a51e0a66b13
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-12-11 10:28:18 +0100

    [WIP] Mind coverage for indel counts
    Get intermediate result for VCF files.

--- scripts/count-insertions-and-deletions/vcf.py
+++ scripts/count-insertions-and-deletions/vcf.py
@@ -1,22 +1,27 @@
-# Returns array [chr, pos, type, len]
-def process_vcf_line(line):
-    return None
+import utils
 
-#     line_result = empty_line_result()
-#     if not line.startswith("#"):
-#         vcf_fields = fields = line.split("\t")
-#         if len(vcf_fields) < 10:
-#             return -1
-#         chromosome = vcf_fields[0]
-#         line_result["chromosome"] = chromosome
-#         reference_bases = vcf_fields[3]
-#         alternative_bases = vcf_fields[4]
-#         # Process insertions
-#         if len(alternative_bases) > len(reference_bases):
-#             line_result["counts"]["insertions"] = 1
-#             line_result["counts"]["inserted_bases"] = len(alternative_bases) - len(reference_bases)
-#         # Process deletions
-#         if len(reference_bases) > len(alternative_bases):
-#             line_result["counts"]["deletions"] = 1
-#             line_result["counts"]["deleted_bases"] = len(reference_bases) - len(alternative_bases)
-#     return line_result
+comment_character = "#"
+min_fields = 10
+
+# Returns array of array [chr, pos, type, len]
+def process_vcf_line(line):
+    if not line.startswith(comment_character):
+        fields = utils.get_fields(line, min_fields)
+        if fields == None:
+            return None
+        chromosome = fields[0]
+        position = int(fields[1])
+        type = None
+        length = None
+
+        reference_bases = fields[3]
+        alternative_bases = fields[4]
+        if len(alternative_bases) > len(reference_bases): # Insertion
+            type = utils.insertion_type
+            length = len(alternative_bases) - len(reference_bases)
+        elif len(reference_bases) > len(alternative_bases): # Deletion
+            type = utils.deletion_type
+            length = len(reference_bases) - len(alternative_bases)
+        else:
+            return None
+        return [[chromosome, position, type, length]] # for compatibility
